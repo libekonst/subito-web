d8e61e24d8c096bfec833ddf5597fb9db6711152
Kostas Liberopoulos <libekonst@gmail.com>
2019-03-16 01:32:09 +0200

    Setup environment.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={style.App}>
+        <header className={ style.App__header}>
+          <img src={logo} className={style.App__logo} alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className="App__link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
